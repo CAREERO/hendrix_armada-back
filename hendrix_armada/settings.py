e692d0b6484a48094eff6b72eca40fff348e09fc
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2024-03-22 10:38:11 +0800

    updated the allowed_host-addlocalhost

--- hendrix_armada/settings.py
+++ hendrix_armada/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.203.95.229', 'localhost']
+ALLOWED_HOSTS = ['52.203.95.229', 'localhost', '127.0.0.1']
 
 # Application definition
 

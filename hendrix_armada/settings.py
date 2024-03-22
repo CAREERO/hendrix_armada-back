c7d76b11c463851521890dce02942db380f9aeb1
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2024-03-22 08:17:48 +0800

    allowed_host updated

--- hendrix_armada/settings.py
+++ hendrix_armada/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.193.37.74']
+ALLOWED_HOSTS = ['52.203.95.229', 'localhost']
 
 
 # Application definition
@@ -165,7 +165,7 @@ STRIPE_TEST_SECRET_KEY=os.environ.get('STRIPE_TEST_SECRET_KEY')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'http://35.170.158.252/static/'
+STATIC_URL = 'http://34.193.37.74/static/'
 MEDIA_URL = '/images/' # to load media files on admin page as well (changes in url.py file also, project level)
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new

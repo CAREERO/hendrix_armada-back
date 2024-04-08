20bdb9feae55eed0b6996027c274721bc3e36628
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2024-04-08 08:01:37 +0800

    updated the authorization

--- payments/views.py
+++ payments/views.py
@@ -200,7 +200,7 @@ class CreateCheckoutSession(APIView):
         shipping_price = math.ceil(shipping_price * 100)
 
         try:
-            YOUR_DOMAIN = 'https://www.hendrixapi.world/'
+            YOUR_DOMAIN = 'https://www.hendrix.world/'
             checkout_session = stripe.checkout.Session.create(
                 payment_method_types=['card'],
                 line_items=[
